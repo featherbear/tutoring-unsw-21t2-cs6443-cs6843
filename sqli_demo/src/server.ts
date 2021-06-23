fd00d949ecbd19ac7c4d91b3cc8a2ffe446557dd
Andrew Wong <andrew.j.wong@outlook.com>
2021-06-24 02:53:32 +1000

    Finish

--- sqli_demo/src/server.ts
+++ sqli_demo/src/server.ts
@@ -1,3 +1,4 @@
+//#region imports
 import polka from 'polka'
 import redirect from '@polka/redirect'
 import bodyParser from 'body-parser'
@@ -6,44 +7,90 @@ import fs from 'fs'
 import { createQuery, init } from './database'
 import crypt from './dodgyCrypto'
 
-const server = polka()
+//#endregion
 
+const server = polka()
 let db = init();
 
-server.use(bodyParser.json())
+function createDoLoginHandler(queryCreator: (username, password) => string, silent: boolean = false) {
+    return (req, res) => {
+        const { username, password } = req.body
+        const OK = (data) => res.end(JSON.stringify({ status: true, auth: data }))
+        const FAIL = (err) => res.end(JSON.stringify({ status: false, message: err }))
 
-server.post('/doLogin', (req, res) => {
-    const { username, password } = req.body
+        const query = queryCreator(username, password)
 
-    const OK = (data) => res.end(JSON.stringify({ status: true, auth: data }))
-    const FAIL = (err) => res.end(JSON.stringify({ status: false, message: err }))
-
-    let query = createQuery`SELECT username, password FROM users WHERE username = '${username}' AND password = '${password}'`
-
-    try {
-        let result = db.prepare(query).get()
-        if (result) {
-            return OK(crypt.encrypt(result))
+        try {
+            let result = db.prepare(query).get()
+            if (result) return OK(crypt.encrypt(result))
+        } catch (e) {
+            console.warn("SQL Error logged - " + e.toString());
+            if (!silent) return FAIL(e.toString())
         }
-    } catch (e) {
-        return FAIL(e.toString())
+
+        return FAIL("Username or password incorrect")
     }
+}
 
-    return FAIL("Username or password incorrect")
-})
+server.post('/doLogin/t1', createDoLoginHandler((username, password) =>
+    createQuery`SELECT username, password FROM users WHERE username = '${username}' AND password = '${password}'`))
 
+server.post('/doLogin/t2', createDoLoginHandler((username, password) =>
+    createQuery`SELECT username, password FROM users WHERE (username = '${username}' AND password = '${password}')`))
+
+server.post('/doLogin/t3', createDoLoginHandler((username, password) =>
+    createQuery`SELECT username, password FROM users WHERE username = '${username.replace(/OR/gi, '')}' AND password = '${password.replace(/OR/gi, '')}'`, true))
+
+
+//#region Server
+server.use(bodyParser.json())
 server.use(cookieParser())
 
 server.get('/', (req, res) => {
-    res.end(fs.readFileSync('site/_root.html').toString().replace("${USERNAME}", res['auth'].username))
+    let username = res['auth'].username
+    if (username == "USER_DOESNT_EXIST") username += "... wait how did you get here?"
+    res.end(fs.readFileSync('site/_root.html').toString().replace("${USERNAME}", username))
 })
 
 server.get('/login', (req, res) => {
     res.end(fs.readFileSync('site/login.html'))
 })
 
+server.get('/playground', (req, res) => {
+    res.end(fs.readFileSync('site/playground.html'))
+})
+
+
+server.post('/playground', (req, res) => {
+    const { query: queryString } = req.body
+
+    let query = createQuery(queryString)
+
+    try {
+        let result = db.prepare(query).all()
+        return res.end(JSON.stringify({ data: result }))
+    } catch (e) {
+        if (e.message === "This statement does not return data. Use run() instead") {
+            try {
+                let result = db.prepare(query).run()
+                return res.end(JSON.stringify({ data: result }))
+            } catch (e) {
+                console.warn("SQL Error logged - " + e.toString());
+                return res.end(JSON.stringify({ data: e.toString() }))
+            }
+        }
+        console.warn("SQL Error logged - " + e.toString());
+        return res.end(JSON.stringify({ data: e.toString() }))
+    }
+})
+
+
+/**
+ * Auth
+ */
 server.use((req, res, next) => {
-    if (["/login", "/doLogin"].includes(req.path)) return next()
+    if (["/login", "/playground"].includes(req.path)) return next()
+    if (req.path.startsWith('/doLogin/')) return next()
 
     const { auth: authCookie } = req.cookies
     if (!authCookie) return redirect(res, "/login")
@@ -61,3 +108,4 @@ export default function listen() {
         console.info(`Server listening on ${HOST}:${PORT}`)
     })
 }
+//#endregion
