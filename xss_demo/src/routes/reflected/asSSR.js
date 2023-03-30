674ecf26677443b39ce318b2cf3e7c12ac27938e
Andrew Wong <andrew.j.wong@outlook.com>
2023-03-30 23:55:32 +1100

    Move demo

--- xss_demo/src/routes/reflected/asSSR.js
+++
@@ -1,9 +0,0 @@
-import Result from './Result.svelte'
-
-export function get(req, res) {
-    let ssr = Result.render({
-        query: req.query.q
-    })
-
-    return res.end(JSON.stringify(ssr))
-}
